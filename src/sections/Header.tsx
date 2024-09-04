b17afa9273be45c20af0caef65fffb3391625bef
tomvj1258 <tomvj15@gmail.com>
2024-09-04 18:27:47 +0530

    Feast: Header section  navbar

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,22 @@
+import { navItemData } from "@/data/data";
+import Link from "next/link";
+
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <>
+      <div className="relative top-3 flex items-center justify-center">
+        <nav className="flex gap-1 rounded-full border border-white/15 bg-white/10 p-0.5 backdrop-blur">
+          {navItemData.map((nav) => (
+            <Link
+              key={nav.name}
+              href={nav.link}
+              className={nav.isColor ? "nav-item nav-item-active" : "nav-item"}
+            >
+              {nav.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
 };
