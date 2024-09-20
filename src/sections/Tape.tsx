0949a1798f10574372b0447fd438e7bdd8201632
tomvj1258 <tomvj15@gmail.com>
2024-09-20 18:07:04 +0530

    Feast: Project & Tape animation added

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,4 +1,5 @@
 import StarIcon from "@/assets/icons/star.svg";
+import { Fragment } from "react";
 const words = [
   "Performant",
   "Accessible",
@@ -18,17 +19,21 @@ export const TapeSection = () => {
     <section className="overflow-x-clip py-16 lg:py-24">
       <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none items-center justify-center gap-4 py-3">
-            {words.map((word) => (
-              <div
-                className="inline-flex items-center justify-center gap-4"
-                key={word}
-              >
-                <span className="text-sm font-extrabold uppercase text-gray-900">
-                  {word}
-                </span>
-                <StarIcon className="size-8 -rotate-12 text-gray-900" />
-              </div>
+          <div className="animate-move-left flex flex-none items-center justify-center gap-4 py-3 pr-4 [animation-duration:30s]">
+            {[...new Array(2)].fill(0).map((_, i) => (
+              <Fragment key={i}>
+                {words.map((word) => (
+                  <div
+                    className="inline-flex items-center justify-center gap-4"
+                    key={word}
+                  >
+                    <span className="text-sm font-extrabold uppercase text-gray-900">
+                      {word}
+                    </span>
+                    <StarIcon className="size-8 -rotate-12 text-gray-900" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
       },
       animation: {
         "ping-large": "ping-large 1s ease-in-out infinite",
+        "move-left": "move-left 1s linear infinite",
       },
 
       keyframes: {
@@ -35,6 +36,15 @@ const config: Config = {
           "75%, 100%": {
             transform: "scale(3)",
             opacity: "0",
+          },
+        },
+
+        "move-left": {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-50%)",
           },
         },
       },
