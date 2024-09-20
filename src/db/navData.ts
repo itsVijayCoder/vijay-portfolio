df2d9a47c4c260362ee668dd9753cfca01b16c04
tomvj1258 <tomvj15@gmail.com>
2024-09-20 14:01:01 +0530

    Fixed: side up arrow added & smooth scroll

--- src/db/navData.ts
+++ src/db/navData.ts
@@ -1,6 +1,6 @@
 export const navItemLinks = [
-  { name: "Home", link: "/", isActive: true },
-  { name: "Projects", link: "#projects", isActive: false },
-  { name: "About", link: "#about", isActive: false },
-  { name: "Contact", link: "#contact", isActive: false },
+  { name: "Home", link: "home", isActive: true },
+  { name: "Projects", link: "projects", isActive: false },
+  { name: "About", link: "about", isActive: false },
+  { name: "Contact", link: "contact", isActive: false },
 ];
